33e9794cb5b5c7d4c1164b0f0e2516a507e8cd32
Vanessa White <whitevanessa14@gmail.com>
2015-03-11 21:52:05 -0400

    Quantile and Boxplot

---
+++ box1.R
@@ -0,0 +1,9 @@
+crayfish <- read.csv("s2040DE_W15_crayfish.csv", header=TRUE)
+boxplot(crayfish[,1], xlab = "Astacus Leptodactylus Crayfish", ylab = "Carapace Length")
+
+## quantile-quantile normal graph
+qqnorm(crayfish[,1])
+## mean <- mean(crayfish[,1])
+## sd <- sd(crayfish[,1])
+## var <- var(crayfish[,1])
+## summary <- summary(crayfish[,1])
